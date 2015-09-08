c3e3b24d10a6b5551ce403d2910e7b5532bcf34e
manish <25manishsh@gmail.com>
2015-09-08 13:33:48 +0530

    site live

---
+++ js/contactus.js
@@ -0,0 +1,49 @@
+$(function() {
+	$(document).ready(function(){
+		var $form = $("#contact_us_form"),
+		fb = new Firebase("https://altus-site.firebaseio.com/"),
+		$msg = $('#msg');
+		$form.validate({
+			rules: {
+				n: {
+					required: true
+				},
+				e: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				n: {
+					required: 'Name is required.'
+				},
+				e: {
+					required: 'Email ID is required.',
+					email: 'Please enter valid Email ID.'
+				}
+			},
+			submitHandler: function (form) {
+				var dataArr = $(form).serializeArray(), dataJson = {};
+				for(var d in dataArr) {
+					dataJson[dataArr[d]['name']] = dataArr[d]['value'];
+				}
+				dataJson['t'] = (new Date()).getTime();
+			    fb.push(dataJson);
+				form.reset();
+				$msg.message({
+					type: 'success',
+					html: 'Your request is submitted successfully.',
+					autoHide: 4000
+				});
+				return false;
+			},
+			invalidHandler: function (event, validator) {
+				$msg.message({
+					type: 'danger',
+					html: 'Submitted form is invalid.',
+					autoHide: 4000
+				});
+			}
+		});
+	});
+});
